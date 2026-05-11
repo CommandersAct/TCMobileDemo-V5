15a36d1958e3c80271eb211a24dcbd26b827d467
Abdelhakim SAID <abdelhakim.said@commandersact.com>
2026-05-11 14:32:07 +0200

    firebase demo

--- Android/TCDemo/app/src/IAB_Firebase/java/com/tagcommander/tcdemo/TCExample/TCFirebaseImplementation.kt
+++ Android/TCDemo/app/src/IAB_Firebase/java/com/tagcommander/tcdemo/TCExample/TCFirebaseImplementation.kt
@@ -1,11 +1,14 @@
 package com.tagcommander.tcdemo.TCExample
 
 import android.content.Context
+import com.google.firebase.FirebaseApp
 import com.tagcommander.lib.firebasedestination.TCFirebase
 
 object TCFirebaseImplementation
 {
     fun initFirebaseDestination(appContext: Context) {
+        FirebaseApp.initializeApp(appContext)
+
         TCFirebase.getInstance().initialize(appContext)
     }
 }
